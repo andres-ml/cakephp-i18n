2654950a82290fb13109a170de1a2cd4d4ee1274
Andres <andres@menuterraneus.com>
2017-01-18 15:48:20 +0100

    fix __t() with replacement arguments in arglist form

--- src/I18n/functions.php
+++ src/I18n/functions.php
@@ -5,16 +5,15 @@ use Aml\I18n\I18n\I18n;
 if (!function_exists('__t')) {
     
     /**
-     * __() wrapper
+     * __d() wrapper with automatic domain
      *
-     * @param string $singular Text to translate.
-     * @param mixed $args Array with arguments or multiple arguments in function.
      * @return mixed Translated string.
      * @link http://book.cakephp.org/3.0/en/core-libraries/global-constants-and-functions.html#__
      */
-    function __t($singular, $args = null)
+    function __t()
     {
-        return __d(I18n::domain(), $singular, $args);
+        $arguments = array_merge([I18n::domain()], func_get_args());
+        return call_user_func_array('__d', $arguments);
     }
 
 }

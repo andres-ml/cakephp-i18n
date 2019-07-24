1cc1dc22961f0081efbe019dacab4f6302a62957
Andrés <andres@dinantia.com>
2019-07-24 11:55:33 +0200

    fix ExtractShell for 3.7 onwards

--- src/Shell/Task/FullExtractTask.php
+++ src/Shell/Task/FullExtractTask.php
@@ -2,22 +2,70 @@
 
 namespace Aml\I18n\Shell\Task;
 
+use Cake\Console\Shell;
 use Cake\Shell\Task\ExtractTask;
 
 /**
- * Override ExtractTask
- * 
- * <!> Do not use ExtractTask as name since cakephp ignores plugin tasks if core task exists
- * 
+ * @inheritDoc
  */
 class FullExtractTask extends ExtractTask
 {
 
-    protected function _parse($functionName, $map) {
-        if ($functionName === '__') {
-            parent::_parse('__t', $map);
-        }
-        parent::_parse($functionName, $map);
-    }
     
+    /**
+     * Add __t() token
+     * 
+     * @inheritDoc
+     */
+    protected function _extractTokens()
+    {
+        /** @var \Cake\Shell\Helper\ProgressHelper $progress */
+        $progress = $this->helper('progress');
+        $progress->init(['total' => count($this->_files)]);
+        $isVerbose = $this->param('verbose');
+
+        $functions = [
+            '__' => ['singular'],
+            '__t' => ['singular'],
+            '__n' => ['singular', 'plural'],
+            '__d' => ['domain', 'singular'],
+            '__dn' => ['domain', 'singular', 'plural'],
+            '__x' => ['context', 'singular'],
+            '__xn' => ['context', 'singular', 'plural'],
+            '__dx' => ['domain', 'context', 'singular'],
+            '__dxn' => ['domain', 'context', 'singular', 'plural'],
+        ];
+        $pattern = '/(' . implode('|', array_keys($functions)) . ')\s*\(/';
+
+        foreach ($this->_files as $file) {
+            $this->_file = $file;
+            if ($isVerbose) {
+                $this->out(sprintf('Processing %s...', $file), 1, Shell::VERBOSE);
+            }
+
+            $code = file_get_contents($file);
+
+            if (preg_match($pattern, $code) === 1) {
+                $allTokens = token_get_all($code);
+
+                $this->_tokens = [];
+                foreach ($allTokens as $token) {
+                    if (!is_array($token) || ($token[0] !== T_WHITESPACE && $token[0] !== T_INLINE_HTML)) {
+                        $this->_tokens[] = $token;
+                    }
+                }
+                unset($allTokens);
+
+                foreach ($functions as $functionName => $map) {
+                    $this->_parse($functionName, $map);
+                }
+            }
+
+            if (!$isVerbose) {
+                $progress->increment(1);
+                $progress->draw();
+            }
+        }
+    }
+
 }
